0382e23824fe829c7273a0e75b6c52b17956fcdc
jack <jspiratos26@gmail.com>
2024-03-02 14:52:49 -0700

    inspecting the data

--- testing.py
+++ testing.py
@@ -37,6 +37,7 @@ def fix_range(df_temp):
 
 
 def make_df(day):
+    print('day : '+ str(day))
     # preserve
     # what we will return
     df_returned = pd.DataFrame()
@@ -50,6 +51,7 @@ def make_df(day):
     df_coral['value'] = -df_coral['value']
     # append into return df
     df_returned = df_returned._append(df_coral,ignore_index=True)
+    print('coral : ', df_coral['value'].min())
     
     
     #repeat
@@ -60,17 +62,21 @@ def make_df(day):
     df_helium = fix_range(df_helium)
     df_returned = df_returned._append(df_helium,ignore_index=True)
     
+    print('helium : ', df_helium['value'].max())
+    
     # obtain
     path = "data/oil_data_day_" + str(day) + ".csv"
     df_oil = pd.read_csv(path)
     df_oil = fix_range(df_oil)
     df_returned = df_returned._append(df_oil,ignore_index=True)
+    print('oil : ', df_oil['value'].max())
     
-    # obtain
+    # obtain or perserve
     path = "data/ship_data_day_" + str(day) + ".csv"
     df_ship = pd.read_csv(path)
     df_ship = fix_range(df_ship)
     df_returned = df_returned._append(df_ship,ignore_index=True)
+    print('ship : ', df_ship['value'].max())
     
     # preserve
     path = "data/species_data_day_" + str(day) + ".csv"
@@ -79,20 +85,24 @@ def make_df(day):
     # since preserve values should be (-)
     df_species['value'] = -df_species['value']
     df_returned = df_returned._append(df_species,ignore_index=True)
+    print('species : ', df_species['value'].min())
     
     # obtain
     path = "data/metal_data_day_" + str(day) + ".csv"
     df_metal = pd.read_csv(path)
     df_metal = fix_range(df_metal)
+    print('metal : ', df_metal['value'].max())
+   
     
     df_returned = df_returned._append(df_metal,ignore_index=True)
     
     return df_returned
     
 
-df_testing = make_df(1)
+
 
 #make files of all types per day 
+
 
 df_day_1 = make_df(1)
 df_day_2 = make_df(2)
@@ -124,24 +134,135 @@ df_day_27 = make_df(27)
 df_day_28 = make_df(28)
 df_day_29 = make_df(29)
 df_day_30 = make_df(30)
+
+
+
+
+# helium maxing 
+
+# x : 0,3
+# y : 0,3
+
+def helium_maxing(day):
+    
+    # obtain
+    path = "data/helium_data_day_" + str(day) + ".csv"
+    df_hel = pd.read_csv(path)
+    df_hel = fix_range(df_hel)
+    
+    df_hel = df_hel[df_hel['x'] < 4]
+    df_hel = df_hel[df_hel['y'] < 4]
+    
+    print('helium : ', df_hel['value'].max())
+    
+    return df_hel
+
     
 
 
-    
-    
 
     
+#df_hell = helium_maxing(3)
 
 
 
 
+# i want to know what to perserve around the helium 
+
+'''
+# coral
+df_coral = pd.DataFrame()
+for i in range(30):
+    # ele 1 is 0 ele 30 is 29
+    j = i+1
+    path = "data/coral_data_day_" + str(j) + ".csv"
+    df_temp = pd.read_csv(path)
+    
+    # fix the range
+    # step 1 find the min value
+    # step 2 either increase/decrease every value equaly such that min is zero 
+    
+    min_value = df_temp['value'].min()
+    
+    df_temp = df_temp.dropna()
+    if(min_value > 0):
+        df_temp['value'] = df_temp['value'] - abs(min_value)
+    else:
+        df_temp['value'] = df_temp['value'] + abs(min_value)
+        
+    df_temp = df_temp[df_temp['x'] < 4]
+    df_temp = df_temp[df_temp['y'] < 4]
+        
+    #add it to the mega file for ressource 
+    
+    df_temp.insert(4,"day", j)
+    
+    df_coral = df_coral._append(df_temp,ignore_index=True)
 
 
+# species
+df_species = pd.DataFrame()
+for i in range(30):
+    # ele 1 is 0 ele 30 is 29
+    j = i+1
+    path = "data/coral_data_day_" + str(j) + ".csv"
+    df_temp = pd.read_csv(path)
+    
+    # fix the range
+    # step 1 find the min value
+    # step 2 either increase/decrease every value equaly such that min is zero 
+    
+    min_value = df_temp['value'].min()
+    
+    df_temp = df_temp.dropna()
+    if(min_value > 0):
+        df_temp['value'] = df_temp['value'] - abs(min_value)
+    else:
+        df_temp['value'] = df_temp['value'] + abs(min_value)
+    
+    df_temp_species = df_temp
+        
+    df_temp = df_temp[df_temp['x'] < 4]
+    df_temp = df_temp[df_temp['y'] < 4]
+        
+    #add it to the mega file for ressource 
+    
+    df_temp.insert(4,"day", j)
+    
+    df_species = df_species._append(df_temp,ignore_index=True)
+    
+    
+# ships
+df_ships = pd.DataFrame()
+for i in range(30):
+    # ele 1 is 0 ele 30 is 29
+    j = i+1
+    path = "data/coral_data_day_" + str(j) + ".csv"
+    df_temp = pd.read_csv(path)
+    
+    # fix the range
+    # step 1 find the min value
+    # step 2 either increase/decrease every value equaly such that min is zero 
+    
+    min_value = df_temp['value'].min()
+    
+    df_temp = df_temp.dropna()
+    if(min_value > 0):
+        df_temp['value'] = df_temp['value'] - abs(min_value)
+    else:
+        df_temp['value'] = df_temp['value'] + abs(min_value)
+        
+    df_temp = df_temp[df_temp['x'] < 4]
+    df_temp = df_temp[df_temp['y'] < 4]
+        
+    #add it to the mega file for ressource 
+    
+    df_temp.insert(4,"day", j)
+    
+    df_ships = df_ships._append(df_temp,ignore_index=True)
 
 
-
-
-
+'''
 
 
 
