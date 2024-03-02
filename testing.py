4b774097cddca4a05e2eb312b772c84a94b0bec1
jack <jspiratos26@gmail.com>
2024-03-02 11:09:36 -0700

    quick test

--- testing.py
+++ testing.py
@@ -1,1 +1,14 @@
 
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+data = pd.read_csv("data/algal_data_day_1.csv")
+
+df_pivot = data.pivot_table(index='x', 
+                          columns='y', 
+                          values='value', 
+                          aggfunc='mean').sort_index(ascending=False)
+
+sns.heatmap(df_pivot, annot=True, cmap='coolwarm')
