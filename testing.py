ac5f2a030d2b4c5959867b147b7ae0deed9eed63
jack <jspiratos26@gmail.com>
2024-03-02 13:33:42 -0700

    make df for everyday

--- testing.py
+++ testing.py
@@ -23,166 +23,110 @@ sns.heatmap(df_pivot, annot=True, cmap='coolwarm')
 
 
 
+def fix_range(df_temp):
+    min_value = df_temp['value'].min()
+    
+    df_temp = df_temp.dropna()
+    if(min_value > 0):
+        df_temp['value'] = df_temp['value'] - abs(min_value)
+    else:
+        df_temp['value'] = df_temp['value'] + abs(min_value)
+        
+    return df_temp
 
 
 
-# algal
-df_algal = pd.DataFrame()
-for i in range(30):
-    # ele 1 is 0 ele 30 is 29
-    j = i+1
-    path = "data/algal_data_day_" + str(j) + ".csv"
-    df_temp = pd.read_csv(path)
+def make_df(day):
+    # what we will return
+    df_returned = pd.DataFrame()
     
-    # fix the range
-    # step 1 find the min value
-    # step 2 either increase/decrease every value equaly such that min is zero 
+    # get path
+    path = "data/coral_data_day_" + str(day) + ".csv"
+    df_coral = pd.read_csv(path)
+    # sort data
+    df_coral = fix_range(df_coral)
+    #append into return df
+    df_returned = df_returned._append(df_coral,ignore_index=True)
     
-    min_value = df_temp['value'].min()
     
-    df_temp = df_temp.dropna()
-    if(min_value > 0):
-        df_temp['value'] = df_temp['value'] - abs(min_value)
-    else:
-        df_temp['value'] = df_temp['value'] + abs(min_value)
-        
-    #add it to the mega file for ressource 
+    #repeat
     
-    df_temp.insert(4,"day", j)
+    path = "data/helium_data_day_" + str(day) + ".csv"
+    df_helium = pd.read_csv(path)
+    df_helium = fix_range(df_helium)
+    df_returned = df_returned._append(df_helium,ignore_index=True)
     
-    df_algal = df_algal._append(df_temp,ignore_index=True)
+    path = "data/oil_data_day_" + str(day) + ".csv"
+    df_oil = pd.read_csv(path)
+    df_oil = fix_range(df_oil)
+    df_returned = df_returned._append(df_oil,ignore_index=True)
     
-# coral
-df_coral = pd.DataFrame()
-for i in range(30):
-    # ele 1 is 0 ele 30 is 29
-    j = i+1
-    path = "data/coral_data_day_" + str(j) + ".csv"
-    df_temp = pd.read_csv(path)
+    path = "data/ship_data_day_" + str(day) + ".csv"
+    df_ship = pd.read_csv(path)
+    df_ship = fix_range(df_ship)
+    df_returned = df_returned._append(df_ship,ignore_index=True)
     
-    # fix the range
-    # step 1 find the min value
-    # step 2 either increase/decrease every value equaly such that min is zero 
+    path = "data/species_data_day_" + str(day) + ".csv"
+    df_species = pd.read_csv(path)
+    df_species = fix_range(df_species)
+    df_returned = df_returned._append(df_species,ignore_index=True)
     
-    min_value = df_temp['value'].min()
+    path = "data/metal_data_day_" + str(day) + ".csv"
+    df_metal = pd.read_csv(path)
+    df_metal = fix_range(df_metal)
+    df_returned = df_returned._append(df_metal,ignore_index=True)
     
-    df_temp = df_temp.dropna()
-    if(min_value > 0):
-        df_temp['value'] = df_temp['value'] - abs(min_value)
-    else:
-        df_temp['value'] = df_temp['value'] + abs(min_value)
-        
-    #add it to the mega file for ressource 
+    return df_returned
     
-    df_temp.insert(4,"day", j)
-    
-    df_coral = df_coral._append(df_temp,ignore_index=True)
-    
-# helium
-df_helium = pd.DataFrame()
-for i in range(30):
-    # ele 1 is 0 ele 30 is 29
-    j = i+1
-    path = "data/helium_data_day_" + str(j) + ".csv"
-    df_temp = pd.read_csv(path)
-    
-    # fix the range
-    # step 1 find the min value
-    # step 2 either increase/decrease every value equaly such that min is zero 
-    
-    min_value = df_temp['value'].min()
-    
-    df_temp = df_temp.dropna()
-    if(min_value > 0):
-        df_temp['value'] = df_temp['value'] - abs(min_value)
-    else:
-        df_temp['value'] = df_temp['value'] + abs(min_value)
-        
-    #add it to the mega file for ressource 
-    
-    df_temp.insert(4,"day", j)
-    
-    df_helium = df_helium._append(df_temp,ignore_index=True)
-    
-# oil
-df_oil = pd.DataFrame()
-for i in range(30):
-    # ele 1 is 0 ele 30 is 29
-    j = i+1
-    path = "data/oil_data_day_" + str(j) + ".csv"
-    df_temp = pd.read_csv(path)
-    
-    # fix the range
-    # step 1 find the min value
-    # step 2 either increase/decrease every value equaly such that min is zero 
-    
-    min_value = df_temp['value'].min()
-    
-    df_temp = df_temp.dropna()
-    if(min_value > 0):
-        df_temp['value'] = df_temp['value'] - abs(min_value)
-    else:
-        df_temp['value'] = df_temp['value'] + abs(min_value)
-        
-    #add it to the mega file for ressource 
-    
-    df_temp.insert(4,"day", j)
-    
-    df_oil = df_oil._append(df_temp,ignore_index=True)
 
-# ship
-df_ship = pd.DataFrame()
-for i in range(30):
-    # ele 1 is 0 ele 30 is 29
-    j = i+1
-    path = "data/ship_data_day_" + str(j) + ".csv"
-    df_temp = pd.read_csv(path)
+df_testing = make_df(1)
+
+#make files of all types per day 
+
+df_day_1 = make_df(1)
+df_day_2 = make_df(2)
+df_day_3 = make_df(3)
+df_day_4 = make_df(4)
+df_day_5 = make_df(5)
+df_day_6 = make_df(6)
+df_day_7 = make_df(7)
+df_day_8 = make_df(8)
+df_day_9 = make_df(9)
+df_day_10 = make_df(10)
+df_day_11 = make_df(11)
+df_day_12 = make_df(12)
+df_day_13 = make_df(13)
+df_day_14 = make_df(14)
+df_day_15 = make_df(15)
+df_day_16 = make_df(16)
+df_day_17 = make_df(17)
+df_day_18 = make_df(18)
+df_day_19 = make_df(19)
+df_day_20 = make_df(20)
+df_day_21 = make_df(21)
+df_day_22 = make_df(22)
+df_day_23 = make_df(23)
+df_day_24 = make_df(24)
+df_day_25 = make_df(25)
+df_day_26 = make_df(26)
+df_day_27 = make_df(27)
+df_day_28 = make_df(28)
+df_day_29 = make_df(29)
+df_day_30 = make_df(30)
     
-    # fix the range
-    # step 1 find the min value
-    # step 2 either increase/decrease every value equaly such that min is zero 
-    
-    min_value = df_temp['value'].min()
-    
-    df_temp = df_temp.dropna()
-    if(min_value > 0):
-        df_temp['value'] = df_temp['value'] - abs(min_value)
-    else:
-        df_temp['value'] = df_temp['value'] + abs(min_value)
-        
-    #add it to the mega file for ressource 
-    
-    df_temp.insert(4,"day", j)
-    
-    df_ship = df_ship._append(df_temp,ignore_index=True)
-    
-# species
-df_species = pd.DataFrame()
-for i in range(30):
-    # ele 1 is 0 ele 30 is 29
-    j = i+1
-    path = "data/species_data_day_" + str(j) + ".csv"
-    df_temp = pd.read_csv(path)
-    
-    # fix the range
-    # step 1 find the min value
-    # step 2 either increase/decrease every value equaly such that min is zero 
-    
-    min_value = df_temp['value'].min()
-    
-    df_temp = df_temp.dropna()
-    if(min_value > 0):
-        df_temp['value'] = df_temp['value'] - abs(min_value)
-    else:
-        df_temp['value'] = df_temp['value'] + abs(min_value)
-        
-    #add it to the mega file for ressource 
-    
-    df_temp.insert(4,"day", j)
-    
-    df_species = df_species._append(df_temp,ignore_index=True)
+
+
     
     
+
+    
+
+
+
+
+
+
+
 
 
 
